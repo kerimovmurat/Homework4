f93e0407a1e4b51ce725bf29cdccb243dcd2ebd8
kerimovmurat <kerimovmurat@yandex.ru>
2024-07-08 14:26:27 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,15 @@
+# Вывести количество символов введённого текста.
+my_string = input()
+# print(my_string)
+k= len(my_string)
+print(k)
+# Выведите строку my_string в верхнем регистре.
+print(my_string.upper())
+# Выведите строку my_string в нижнем регистре.
+print(my_string.lower())
+# Измените строку my_string, удалив все пробелы.
+print(my_string.replace(" ",""))
+# Выведите первый символ строки my_string
+print(my_string[0])
+# Выведите последний символ строки my_string.
+print(my_string[-1])
